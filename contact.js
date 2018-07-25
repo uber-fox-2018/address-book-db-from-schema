dfe89005d56a4100b460d715b252a17e5ad30af4
talkasrul <talkasrul@gmail.com>
2018-07-25 20:31:25 +0700

    find contact in promise

---
+++ contact.js
@@ -0,0 +1,82 @@
+const db = require('./config.js');
+
+class Contact {
+    
+    constructor(id, name, company, phone, email) {
+        this.table = 'contacts';
+        this.id = id;
+        this.name = name;
+        this.company = company;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    addContact(name, company, phone, email) {
+        this.name = name;
+        this.company = company;
+        this.phone = phone;
+        this.email = email;
+        
+        return new Promise((resolve, reject)=> {
+            let query = `INSERT INTO ${this.table}(name, company, phoneNumber, email)
+                    VALUES("${this.name}", "${this.company}", "${this.phone}", "${this.email}")`;
+            db.run(query, function(err) {
+                if(!err) {
+                    let id = this.lastID;
+                    resolve(id)
+                } else {
+                    reject(err)
+                }              
+            })
+        })
+    }
+
+    contactList(cb) {
+        let query = `SELECT * FROM ${this.table}`;
+        db.all(query, (err, data) => {
+            cb(data)
+        })
+    }
+
+    updateContact(id, name, company, phone, email) {
+        this.id = id;
+        this.name = name;
+        this.company = company;
+        this.phone = phone;
+        this.email = email;
+
+        let query = `UPDATE ${ this.table } SET 
+            name = "${ this.name }", company = "${ this.company }", 
+            phoneNumber = "${ this.phone }", email = "${ this.email }"  WHERE id = "${ this.id }"`;
+		db.run(query, (err) => { })
+    }
+
+    deleteContact(id) {
+        this.id = id;
+        let query = `DELETE FROM ${ this.table }  WHERE id = "${ this.id }"`;
+        return new Promise((resolve, reject)=> {
+            db.run(query, (err) => { 
+                
+            })
+        })
+		
+    }
+
+    findContact(id) {
+        this.id = id;
+        let query = `SELECT * FROM ${this.table} WHERE id = ${this.id}`;
+        return new Promise((resolve, reject)=> {
+            db.get(query, (err, data) => {
+                if(err) {
+                    reject(err)
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+        
+    }
+
+}
+
+module.exports = Contact
--- main.js
+++ main.js
@@ -1,1 +1,201 @@
+const argv = process.argv.slice(2);
 
+const MContact = require('./contact.js');
+const MGroup = require('./group.js');
+const MContactGroup = require('./contact-group.js');
+const View = require('./view.js')
+
+class Controller {
+    constructor(input) {
+        this._argv = input;
+        this.modelContact = new MContact;
+        this.modelGroup = new MGroup;
+        this.modelContactGroup = new MContactGroup;
+        this.routes();
+    }
+
+    // add contact in promise
+    addContact() {
+        let name    = this._argv[1];
+        let company = this._argv[2];
+        let phone   = this._argv[3];
+        let email   = this._argv[4];
+
+        this.modelContact.addContact(name, company, phone, email)
+        .then((id) => {
+            this.modelContact.findContact(id)
+            .then((data) => {
+                View.Display(data)
+            })
+            .catch((err)=> {
+                View.Display(err)
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+        
+    }
+
+    contactList() {
+        this.modelContact.contactList( (data) => {
+            console.log(data);
+        })
+    }
+
+    updateContact() {
+        let id      = this._argv[1];
+        let name    = this._argv[2];
+        let company = this._argv[3];
+        let phone   = this._argv[4];
+        let email   = this._argv[5];
+        this.modelContact.updateContact(id, name, company, phone, email);
+        let msg = `Berhasil`;
+        View.Display(msg)
+    }
+
+    
+    deleteContact() {
+        let id      = this._argv[1];
+        this.modelContact.deleteContact(id);
+        let msg = `Berhasil`;
+        View.Display(msg)
+    }
+
+    // find contact in promise
+    findContact() {
+        let id = this._argv[1];
+        this.modelContact.findContact(id)
+            .then((data) => {
+                View.Display(data)
+            })
+            .catch((err)=> {
+                View.Display(err)
+            })
+    }
+
+    ///////
+
+    addGroup() {
+        let name    = this._argv[1];
+
+        this.modelGroup.addContactGroup(name);
+        let msg = `Berhasil`;
+        View.Display(msg)
+    }
+
+    groupList() {
+        this.modelGroup.groupList( (data) => {
+            console.log(data);
+        })
+    }
+
+    updateGroup() {
+        let id      = this._argv[1];
+        let name    = this._argv[2];
+        
+        this.modelGroup.updateGroup(id, name);
+        let msg = `Berhasil`;
+        View.Display(msg)
+    }
+
+    deleteGroup() {
+        let id      = this._argv[1];
+        this.modelGroup.deleteGroup(id);
+        let msg = `Berhasil`;
+        View.Display(msg)
+    }
+
+    ////////////////
+
+    addContactGroup() {
+        let contactId    = this._argv[1];
+        let groupId    = this._argv[1];
+
+        this.modelContactGroup.addContactGroup(contactId, groupId);
+        let msg = `Berhasil`;
+        View.Display(msg)
+    }
+
+    contactGroupList() {
+        this.modelContactGroup.updateContactGroup( (data) => {
+            console.log(data);
+        })
+    }
+
+    updateContactGroup() {
+        let id      = this._argv[1];
+        let contactId    = this._argv[2];
+        let groupId    = this._argv[2];
+        
+        this.modelContactGroup.updateContactGroup(id, contactId, groupId);
+        let msg = `Berhasil`;
+        View.Display(msg)
+    }
+
+    deleteContactGroup() {
+        let id      = this._argv[1];
+        this.modelContactGroup.deleteContactGroup(id);
+        let msg = `Berhasil`;
+        View.Display(msg)
+    }
+
+    help() {
+        View.help()
+    }
+
+    routes() {
+        let cmd = this._argv[0];
+        // console.log(cmd)
+        switch (cmd) {
+            case 'addContact':
+                this.addContact();
+                break;
+            case 'contactList':
+                this.contactList();
+                break;
+            case 'updateContact':
+                this.updateContact();
+                break;
+            case 'deleteContact':
+                this.deleteContact();
+                break;
+            case 'findContact':
+                this.findContact();
+                break;
+                
+            case 'addGroup':
+                this.addGroup();
+                break;
+            case 'groupList':
+                this.groupList();
+                break;
+            case 'updateGroup':
+                this.updateGroup();
+                break;
+            case 'deleteGroup':
+                this.deleteGroup();
+                break;
+
+            case 'addContactGroup':
+                this.addContactGroup();
+                break;
+            case 'contactGroupList':
+                this.contactGroupList();
+                break;
+            case 'updateContactGroup':
+                this.updateContactGroup();
+                break;
+            case 'deleteContactGroup':
+                this.deleteContactGroup();
+                break;
+
+            default:
+                this.help();
+                break;
+        }
+    }
+
+}
+
+new Controller(argv)
